69f27c0456fdb8e1d7f8b263d2e04662982fe7e2
Ivan <ivan.gorynin90@proton.me>
2025-10-14 00:15:37 +0200

    Small refactoring on Person controller

--- src/api/person/controllers/person.js
+++ src/api/person/controllers/person.js
@@ -5,10 +5,10 @@
  */
 
 const { createCoreController } = require('@strapi/strapi').factories;
-const makeProfileFieldController = require('../../../utils/makeProfileFieldController');
 
 const UID = 'api::person.person';
 
+const makeProfileFieldController = require('../../../utils/makeProfileFieldController');
 module.exports = createCoreController(UID, ({ strapi }) => {
   const securedCrudController = makeProfileFieldController({ strapi, UID, typeField: "person" });
   return {
